2f6713f752a8c2a172c1607079bcf59863b69db1
Jason Nichols <jasonnic@gmail.com>
2017-02-03 21:30:05 +0900

    before demo app

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function(request, response){
     response.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    //Hard coding for siplicity.  Pretend this hits database
+    res.json([
+        {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+        {"id": 2, "firstName": "Bob2", "lastName": "Smith2", "email": "bob2@gmail.com"},
+        {"id": 3, "firstName": "Bob3", "lastName": "Smith3", "email": "bob3@gmail.com"},
+        
+    ]);
+});
+
 app.listen(port, function(err){
     if(err){
         console.log(err);
